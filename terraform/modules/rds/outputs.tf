37396fa66ff1bf65562c56e1f876890eb71f16b2
prashansa <joshiprashansa11@gmail.com>
2024-07-23 13:19:29 +0545

    added the endpoint of rds in elastic beanstalk

--- terraform/modules/rds/outputs.tf
+++ terraform/modules/rds/outputs.tf
@@ -1,6 +1,6 @@
 output "db_instance_endpoint" {
   description = "The endpoint of the RDS instance"
-  value       = aws_db_instance.prashansa_private_db_instance.address
+  value       = aws_db_instance.prashansa_private_db_instance.endpoint
 }
 
 output "db_instance_id" {
